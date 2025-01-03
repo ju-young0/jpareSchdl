c96064e7d94c697f8c7e1ba19a2441f72aac26bb
juyoung <dlaks154@gmail.com>
2025-01-03 11:33:33 +0900

    feat : init

---
+++ src/main/java/org/example/jpareschdl/JpareSchdlApplication.java
@@ -0,0 +1,13 @@
+package org.example.jpareschdl;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpareSchdlApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JpareSchdlApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/jpareschdl/JpareSchdlApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.jpareschdl;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpareSchdlApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
